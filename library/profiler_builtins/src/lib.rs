cc1b58b7593f41a9c7f2da31f75e8a5aac1e0856
Parthasarathy <116651878+parthasarathy-G76@users.noreply.github.com>
2023-10-07 10:18:57 +0530

    Update lib.rs

--- library/profiler_builtins/src/lib.rs
+++ library/profiler_builtins/src/lib.rs
@@ -2,7 +2,7 @@
 #![feature(profiler_runtime)]
 #![profiler_runtime]
 #![unstable(
-    feature = "profiler_runtime_lib",
+    feature = "profiler_runtime_library",
     reason = "internal implementation detail of rustc right now",
     issue = "none"
 )]
